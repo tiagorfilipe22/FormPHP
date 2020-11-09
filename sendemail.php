f05c3abd6a05f650ada688d72c13a93efa08a7d6
tiagorfilipe22 <tiagorfilipe22@hotmail.com>
2020-11-09 20:32:25 +0000

    Update

---
+++ sendemail.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_post['submit'])) {
+       $name = $_post['name'];
+       $subject = $_post['subject'];
+       $mailFrom = $_post['mail'];
+       $message = $_post['message'];
+
+       $mailTo = "tiagorfilipe22@hotmail.com";
+       $headers = "From: ".$mailFrom;
+       $txt = "You have received an email from ".$name. ".\n\n".$message;
+
+       mail($mailTo, $subject, $txt, $headers);
+       header("Location: index.php?mailsend");
+
+       # code...
+}
